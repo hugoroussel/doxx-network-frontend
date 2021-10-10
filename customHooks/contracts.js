1d0bc076547abc566c9367b18b7d9ea2268b6aed
hugoroussel <hroussel96@gmail.com>
2021-10-10 21:58:01 +0200

    Switch to Polygon

--- customHooks/contracts.js
+++ customHooks/contracts.js
@@ -7,8 +7,8 @@ import BigNumber from 'bignumber.js';
 import dnpAbi from '../abis/dnp.js';
 import erc20Abi from '../abis/erc20.js';
 
-const DNP = '0x2BCaf26109197e6695B88B4b693eb2E9D1A53334';
-const DAI = '0x88271d333C72e51516B67f5567c728E702b3eeE8';
+const DNP = '0x03D370812d754021E66926CEF9f420178B60d1fc';
+const DAI = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
 
 function hexToInt(hexstring) {
   return parseInt(hexstring, 16);

72ac282db7e5d193f938f26912cd09b01220f7d6
hugoroussel <hroussel96@gmail.com>
2021-10-10 15:25:32 +0200

    New contract address

--- utils/registry.js
+++ utils/registry.js
@@ -1,4 +1,4 @@
-const dnpAddress = '0xe37bc7AB8db8a062CB40d234ECF0e8b4C6520BdC';
+const dnpAddress = '0x06ccECB4Ca0634caF76ca1F1CBE5ee3316AD04e4';
 const daiAddress = '0x88271d333C72e51516B67f5567c728E702b3eeE8';
 
 module.exports = { dnpAddress, daiAddress };

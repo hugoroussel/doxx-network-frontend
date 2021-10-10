2fcba7ed51a764ecc4135ff308248f90fa1eed4d
hugoroussel <hroussel96@gmail.com>
2021-10-10 15:42:25 +0200

    Prepare for prod

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const path = require('path');
+
 module.exports = {
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve('./'));
+    return config;
+  },
   trailingSlash: true,
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
@@ -8,5 +14,17 @@ module.exports = {
   env: {
     SERVER_IP: process.env.SERVER_IP,
     CHAIN_ID: process.env.CHAIN_ID,
+  },
+  async exportPathMap() {
+    const paths = {
+      '/': { page: '/' },
+      '/account': { page: '/account' },
+      '/check': { page: '/check' },
+      '/register': { page: '/register' },
+      '/search': { page: '/search' },
+      '/bounty': { page: '/bounty' },
+      '/bounty/search': { page: '/bounty/search' },
+    };
+    return paths;
   },
 };
